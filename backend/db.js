9bc775265e007633c7d2c0cd3e9c2d935d73e4a4
Harshil Rana <ranaharshil557@gmail.com>
2024-05-22 16:58:43 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,11 @@ const env = require("dotenv");
 env.config();
 
 const db = new pg.Client({
-  // user: process.env.PG_USER,
-  // host: process.env.PG_HOST,
-  // database: process.env.PG_DATABASE,
-  // password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT, 
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
