9bb3af51a3c7e6ff32d2154ba69019cfa6cb32e7
Harshil Rana <ranaharshil557@gmail.com>
2024-05-22 16:56:32 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const db = new pg.Client({
   // host: process.env.PG_HOST,
   // database: process.env.PG_DATABASE,
   // password: process.env.PG_PASSWORD,
-  // port: process.env.PG_PORT,
+  // port: process.env.PG_PORT, 
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
