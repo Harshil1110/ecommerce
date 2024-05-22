8814004310788b585f16fc6f2555268dba00ecf8
Harshil Rana <ranaharshil557@gmail.com>
2024-05-22 12:58:57 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const user = require("./routes/User");
 const produt = require("./routes/Product");
 const env = require("dotenv");
 env.config();
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT || process.env.SERVER_PORT;
 
 app.use(express.json());
 
